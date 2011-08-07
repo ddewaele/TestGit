0bfaba359ff0b76b2ba2473f560f61ab593fc079
ddewaele <ddewaele@gmail.com>
2011-08-08 00:44:12 +0200

    changed on head while dev branch active

--- src/com/ecs/test/git/ServiceOne.java
+++ src/com/ecs/test/git/ServiceOne.java
@@ -9,7 +9,7 @@ package com.ecs.test.git;
 public class ServiceOne {
 
 	public void doSomethingHereAlso() {
-		System.out.println("doing something here also");
+		System.out.println("doing something here also..........");
 	}
 	
 	public void andHereAlso() {
