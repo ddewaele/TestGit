c654e85d5a061aef4fa58d7dc401e64b79525a09
ddewaele <ddewaele@gmail.com>
2011-08-08 00:22:26 +0200

    and here also

--- src/com/ecs/test/git/ServiceOne.java
+++ src/com/ecs/test/git/ServiceOne.java
@@ -5,4 +5,8 @@ public class ServiceOne {
 	public void doSomethingHereAlso() {
 		System.out.println("doing something here also");
 	}
+	
+	public void andHereAlso() {
+		System.out.println("and here also");
+	}
 }
