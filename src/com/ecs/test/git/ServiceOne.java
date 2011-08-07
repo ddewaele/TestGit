6a8ebabc88e7116b2e3ba092dc754ff6ddfa9a7f
ddewaele <ddewaele@gmail.com>
2011-08-08 00:23:59 +0200

    added docs

--- src/com/ecs/test/git/ServiceOne.java
+++ src/com/ecs/test/git/ServiceOne.java
@@ -1,5 +1,18 @@
 package com.ecs.test.git;
 
+/**
+ * docs 
+ * 
+ * @author Davy
+ *
+ */
 public class ServiceOne {
 
+	public void doSomethingHereAlso() {
+		System.out.println("doing something here also");
+	}
+	
+	public void andHereAlso() {
+		System.out.println("and here also");
+	}
 }
